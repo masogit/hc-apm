23322f2739e7706179fcb8bf44b36a6a78d33b5b
Man, Shuo <shuo.man@gmail.com>
2017-02-22 16:47:38 +0800

    Add a icon for sending message menu

--- src/js/components/TopBar.js
+++ src/js/components/TopBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { AppBar, IconMenu, IconButton, MenuItem, Toggle } from 'material-ui';
-import { SocialPerson, ActionExitToApp, ActionHelp } from 'material-ui/svg-icons';
+import { SocialPerson, ActionExitToApp, ActionHelp, CommunicationMessage } from 'material-ui/svg-icons';
 import { Translate } from 'react-redux-i18n';
 
 export default class TopBar extends Component {
@@ -28,7 +28,7 @@ export default class TopBar extends Component {
       <IconMenu iconButtonElement={<IconButton><SocialPerson /></IconButton>} targetOrigin={{vertical: 'bottom', horizontal: 'left'}}>
           { this.renderThemeMenuItem(header, actions) }
           { this.renderLocaleMenuItem(header, actions) }
-          <MenuItem value="2" primaryText="Send a message" insetChildren onTouchTap={messageSend.bind(this, 'Test send a global message!')}/>
+          <MenuItem leftIcon={<CommunicationMessage />} primaryText="Send a message" insetChildren onTouchTap={messageSend.bind(this, 'Test send a global message!')}/>
           <MenuItem value="3" primaryText="Settings" insetChildren/>
           <MenuItem value="4" primaryText="Help" leftIcon={<ActionHelp />} />
           <MenuItem value="5" primaryText={<Translate value="header.menu.logoff" />} leftIcon={<ActionExitToApp />}/>
