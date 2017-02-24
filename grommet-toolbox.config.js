3c5ffcafc3c340c9105adc29cc701dce9ee493a8
Man, Shuo <shuo.man@gmail.com>
2017-02-24 13:27:12 +0800

    Proxy to local rest to simulate remote rest data

--- grommet-toolbox.config.js
+++ grommet-toolbox.config.js
@@ -10,20 +10,17 @@ export default {
   mainJs: 'src/js/index.js',
   mainScss: 'src/scss/index.scss',
   devServerPort: 9001,
+  devServerProxy: {
+    "/dataget": {
+      target: "http://localhost:9000/sites",
+      // target: "https://geapmuat2.run.aws-jp01-pr.ice.predix.io/web/dataget",
+      secure: false
+    }
+  },
   webpack: {
     loaders: [
       { test: /\.json$/, loader: 'json' }
     ],
-    devServer: {
-      proxy: {
-        "/dataget": {
-          target: "https://geapmuat2.run.aws-jp01-pr.ice.predix.io/web",
-          secure: false,
-          changeOrigin: true,
-          ignorePath: true
-        }
-      }
-    },
     devtool: 'cheap-source-map'
   }
 };
