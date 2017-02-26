303b178e4da1df909ed90495014598edb901d9ef
Man, Shuo <shuo.man@gmail.com>
2017-02-26 21:37:46 +0800

    REST to get site data

--- src/js/actions/rest.js
+++ src/js/actions/rest.js
@@ -1,22 +1,22 @@
-import { TYPE } from '../constants';
-// import { Rest } from 'grommet';
-import sites from '../../../conf/site.json';
-
-export function loadSites() {
-  return dispatch => {
-    dispatch({ type: TYPE.REST_LOAD_SITES, sites });
-  };
-};
+import { TYPE, URL } from '../constants';
+import { Rest } from 'grommet';
+// import sites from '../../../conf/site.json';
 
 // export function loadSites() {
 //   return dispatch => {
-//     Rest.default.get(URL.SITE).then((res) => {
-//       let sites = res.body;
-//       return dispatch({
-//         type: TYPE.REST_LOAD_SITES,
-//         sites
-//       });
-//     });
+//     dispatch({ type: TYPE.REST_LOAD_SITES, sites });
 //   };
 // };
+
+export function loadSites() {
+  return dispatch => {
+    Rest.default.get(URL.SITE).then((res) => {
+      let sites = res.body;
+      return dispatch({
+        type: TYPE.REST_LOAD_SITES,
+        sites
+      });
+    });
+  };
+};
 
