9143a4df0b8575df83a9dcef6f05d152a66e93c7
Man, Shuo <shuo.man@gmail.com>
2017-02-27 15:48:27 +0800

    return promise to calling function

--- src/js/actions/rest.js
+++ src/js/actions/rest.js
@@ -3,7 +3,7 @@ import fetch from 'isomorphic-fetch';
 
 export function loadSites() {
   return dispatch => {
-    fetch(URL.SITE)
+    return fetch(URL.SITE)
       .then(res => (res.status >= 400) ? console.log('http err') : res.json())
       .then(sites => dispatch({type: TYPE.REST_LOAD_SITES, sites}));
   };
